b0b718936f25cd49a7014f281df499db16f34236
samukan <122282437+samukan@users.noreply.github.com>
2023-04-04 23:53:27 +0200

    Delete Tehtava3 directory

--- Tehtava3/tehtava3.js
+++
@@ -1,9 +0,0 @@
-const num1 = +prompt("Anna luku:")
-const num2 = +prompt("Anna toinen luku:")
-const num3 = +prompt("Anna kolmas luku:")
-
-let sum = num1 + num2 + num3;
-let prod =  num1 * num2 * num3;
-let div = (num1 + num2+ num3) /3;
-
-document.getElementById('target').innerHTML = `${sum}, ${prod}, ${div}`;
